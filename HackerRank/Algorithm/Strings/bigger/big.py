19acad09897878061271a1cc44787da85327b147
arunxls <arunxls@gmail.com>
2015-06-16 23:09:16 -0400

    Starting string problems

--- HackerRank/Algorithm/Strings/bigger/big.py
+++ HackerRank/Algorithm/Strings/bigger/big.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 
 def main():
-    for i in range(int(raw_input().strip())):
-        inp = list(reversed(list(raw_input().strip())))
 
-        f = inp[0]
+    cases = int(raw_input().strip())
+    for i in range(cases):
+        inp = list(raw_input().strip())
+        s_inp = sorted(inp, reverse = 1)
 
-        index = 0
-        flag = False
-        for w in inp:
-            if w > f:
-                tmp = inp[index - 1]
-                inp[index - 1] = inp[index]
-                inp[index] = tmp
-                print ''.join(str(x) for x in list(reversed(inp)))
-                flag = True
-                break
-            f = w
-            index += 1
-        if not flag:
+        if ''.join(str(x) for x in inp) == ''.join(str(x) for x in s_inp):
             print "no answer"
+        else:
+            start = 0
+            for i in range(len(inp) - 1):
+                if inp[i] < inp[i+1]:
+                    start = i
 
+            end = 0
+            for j in range(len(inp)):
+                if inp[start] < inp[j]:
+                    end = j
+
+
+            tmp = inp[start]
+            inp[start] = inp[end]
+            inp[end] = tmp
+
+            inp[start+1:] = sorted(inp[start+1:])
+            print ''.join(str(x) for x in inp)
 
 if __name__ == '__main__':
     main()
